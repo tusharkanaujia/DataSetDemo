1c2bfdcb3b1022cd718429f34f7a3e42162c1783
Kanaujia Tushar <Tushar.Kanaujia@uk.mizuho-sc.com>
2021-01-20 17:31:17 +0000

    Draft

---
+++ CalanderManager/CalanderManager/CalanderManager.vb
@@ -0,0 +1,67 @@
+﻿Imports System.Configuration
+Imports System.Data.SqlClient
+
+Module CalanderManager
+
+    Sub Main()
+        Dim conn As SqlConnection
+        Dim cmdCalander As SqlCommand
+        Dim conString As String
+
+        conString = ConfigurationManager.ConnectionStrings("CalanderManagerConnectionString").ConnectionString
+        conn = New SqlConnection(conString)
+        conn.Open()
+        cmdCalander = New SqlCommand
+
+        'USING UNTYPED DATASET
+        Dim adpCalander As SqlDataAdapter = New SqlDataAdapter
+
+        With cmdCalander
+            .Connection = conn
+            .CommandTimeout = 30
+            .CommandType = CommandType.Text
+            .CommandText = "SELECT DateKey, FullDate, IsHoliday FROM Calander"
+        End With
+
+        adpCalander.SelectCommand = cmdCalander
+        Dim dsCalander As DataSet = New DataSet
+        adpCalander.Fill(dsCalander, "Calander")
+
+        cmdCalander.CommandText = "SELECT DateKey, CountryHoliday FROM Holiday"
+        adpCalander.Fill(dsCalander, "Holiday")
+        Dim counter As Int32 = 0
+
+        For Each holiday As DataRow In dsCalander.Tables(1).Rows
+            For Each calander As DataRow In dsCalander.Tables(0).Rows
+                If calander.ItemArray(0) = holiday.ItemArray(0) Then
+                    dsCalander.Tables(0).Rows(counter)(2) = True
+                End If
+                counter = counter + 1
+            Next
+        Next
+        'dsCalander.Tables(0).AcceptChanges()
+        adpCalander.Update(dsCalander, "Calander")
+
+        'USING TYPED DATASET
+        Dim adpCalanderTyped As CalanderDataSetTableAdapters.CalanderTableAdapter = New CalanderDataSetTableAdapters.CalanderTableAdapter
+        Dim CalanderTable As CalanderDataSet.CalanderDataTable = New CalanderDataSet.CalanderDataTable
+        adpCalanderTyped.Fill(CalanderTable)
+
+        Dim adpHolidayTyped As CalanderDataSetTableAdapters.HolidayTableAdapter = New CalanderDataSetTableAdapters.HolidayTableAdapter
+        Dim HolidayTable As CalanderDataSet.HolidayDataTable = New CalanderDataSet.HolidayDataTable
+        adpHolidayTyped.Fill(HolidayTable)
+        counter = 0
+        For Each holiday As DataRow In HolidayTable.Rows
+            For Each calander As DataRow In CalanderTable.Rows
+                If calander.ItemArray(0) = holiday.ItemArray(0) Then
+                    CalanderTable.Rows(counter)(2) = True
+                End If
+                counter = counter + 1
+            Next
+        Next
+        CalanderTable.AcceptChanges()
+        adpCalanderTyped.Update(CalanderTable)
+        conn.Close()
+    End Sub
+
+End Module
---
+++ CalanderManager/CalanderManager/DataBaseScript/DatabaseScripts.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[Calander]
+(
+    [DateKey]   [INT]  NOT NULL
+   ,[FullDate]  [DATE] NOT NULL
+   ,[IsHoliday] [BIT]  NOT NULL
+        CONSTRAINT [DF_dbo.Calander_IsHoliday]
+            DEFAULT ((0))
+) ON [PRIMARY] ;
+GO
+ALTER TABLE [dbo].[Calander]
+ADD CONSTRAINT [PK_Calander]
+    PRIMARY KEY CLUSTERED ([DateKey]) ON [PRIMARY] ;
+GO
+
+CREATE TABLE [dbo].[Holiday]
+(
+    [DateKey]        [INT]         NOT NULL
+   ,[CountryHoliday] [VARCHAR](50) NOT NULL
+) ON [PRIMARY] ;
+GO
+ALTER TABLE [dbo].[Holiday]
+ADD CONSTRAINT [PK_Holiday]
+    PRIMARY KEY CLUSTERED ([DateKey]) ON [PRIMARY] ;
+GO
